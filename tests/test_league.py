58799c246c38b7994d5619a1567d4d58c46729e0
M Chadwick <dr.m.chadwick@gmail.com>
2018-07-06 00:54:47 +0100

    Import League for convenience.

--- tests/test_league.py
+++ tests/test_league.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from sqlalchemy.orm import sessionmaker
 
 from replay_finder.model import InitDB, get_api_usage, Replay, APIUsage
+from replay_finder.model import League
 from replay_finder.league import update_league_listing, update_league_replays
 
 engine = InitDB('sqlite://')
