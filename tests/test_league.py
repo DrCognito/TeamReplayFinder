28d5a9d1b68dc06b48762ac650d472a69505f17c
M Chadwick <dr.m.chadwick@gmail.com>
2018-07-05 16:50:42 +0100

    Test replay match_details retrieval. Import Replay for convenience in interactive mode.

--- tests/test_league.py
+++ tests/test_league.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from sqlalchemy.orm import sessionmaker
 
-from replay_finder.model import InitDB
+from replay_finder.model import InitDB, get_api_usage, Replay, APIUsage
 from replay_finder.league import update_league_listing, update_league_replays
 
 engine = InitDB('sqlite://')
@@ -15,3 +15,4 @@ session = Session()
 test_id = 10049
 
 update_league_listing(session)
+update_league_replays(session, league_id=test_id)
