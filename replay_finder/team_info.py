2a492f8d38ce58636fefda6c0e933866e623880d
M Chadwick <dr.m.chadwick@gmail.com>
2018-07-12 17:17:49 +0100

    Better to use team, player id based players instead of many to many relationship. Full implementation of import from old. Use proper property for date. Removed extra commits for speed. Correct use of os.environ as a dictionary.

--- replay_finder/team_info.py
+++ replay_finder/team_info.py
@@ -1,13 +1,15 @@
+from datetime import datetime, timedelta
 from os import environ as environment
 
-from sqlalchemy import (BigInteger, Column, DateTime, Integer, String,
-                        create_engine)
+from sqlalchemy import (BigInteger, Column, DateTime, ForeignKey, Integer,
+                        String, create_engine)
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship
 
 Base_TI = declarative_base()
+DEFAULT_TIME = datetime.today() - timedelta(days=30)
 
 
 class TeamInfo(Base_TI):
@@ -29,12 +31,13 @@ class TeamInfo(Base_TI):
 class TeamPlayer(Base_TI):
     __tablename__ = "team_players"
     player_id = Column(BigInteger, primary_key=True)
+    team_id = Column(Integer, ForeignKey(TeamInfo.team_id), primary_key=True)
     name = Column(String)
 
 
 def InitTeamDB(path=None):
     if path is None:
-        path = environment("TEAM_DB_PATH")
+        path = environment["TEAM_DB_PATH"]
     engine = create_engine(path, echo=False)
     Base_TI.metadata.create_all(engine)
 
@@ -46,7 +49,7 @@ def build_team(team_id, name, date, players, session):
 
     new_team.team_id = team_id
     new_team.name = name
-    new_team.date = date
+    new_team.last_change = date
     new_team.players = players
 
     try:
@@ -60,28 +63,35 @@ def build_team(team_id, name, date, players, session):
     return new_team
 
 
-def process_player(name, player_id, session):
-    # Check if they exist already, players can be on multiple teams!
-    player = session.query(TeamPlayer).filter(TeamPlayer.player_id == player_id)\
-                                      .one_or_none()
+def process_player(name, player_id, team_id, session):
 
-    if player is None:
-        player = TeamPlayer()
-        player.player_id = player_id
-        player.name = name
+    player = TeamPlayer()
+    player.player_id = player_id
+    player.name = name
+    player.team_id = team_id
 
-        try:
-            session.add(player)
-            session.commit()
-        except SQLAlchemyError:
-            session.rollback()
-            raise
+    try:
+        session.merge(player)
+        # session.commit()
+    except SQLAlchemyError:
+        session.rollback()
+        raise
 
     return player
 
 
-def import_from_old(team_ids, all_players, validity_times):
+def import_from_old(team_ids, all_players, validity_times, session):
     for name in team_ids:
-        if name not in players:
+        if name not in all_players:
             print("Player data for {} missing!".format(name))
             continue
+
+        valid_from = validity_times.get(name, DEFAULT_TIME)
+
+        player_list = []
+        team = all_players[name]
+        for player in team:
+            player_list.append(process_player(player, team[player],
+                                              team_ids[name], session))
+
+        build_team(team_ids[name], name, valid_from, player_list, session)
