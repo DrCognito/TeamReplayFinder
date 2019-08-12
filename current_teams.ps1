def131590531306bb0c138dfaa6cc1a2c365aa13
M Chadwick <dr.m.chadwick@gmail.com>
2019-08-12 16:13:40 +0100

    Processing scripts covering TI2019 teams.

---
+++ current_teams.ps1
@@ -0,0 +1,3 @@
+C:/Users/Cognito/.virtualenvs/TeamReplayFinder-8Br9gCsa/Scripts/activate.ps1
+$teams = 1838315, 1883502, 726228, 39, 2163, 15, 350190, 6214973, 2108395, 2586976, 111474, 2626685, 6214538, 2672298, 7203342, 36, 6209804, 543897
+python .\team_parse.py $teams
