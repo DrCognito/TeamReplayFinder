b1616c74d8b308e457668ab9b20d502bd24357b2
Matt Chadwick <dr.m.chadwick@gmail.com>
2020-07-16 00:05:03 +0100

    Old team processing scripts

--- download_teams.ps1
+++ download_teams.ps1
@@ -1,3 +1,3 @@
 C:/Users/Cognito/.virtualenvs/TeamReplayFinder-8Br9gCsa/Scripts/activate.ps1
-$teams = "Team Secret", "Virtus Pro", "Vici Gaming", "Evil Geniuses", "Team Liquid", "PSG.LGD", "Fnatic", "Ninjas in Pyjamas", "TNC Predator", "OG", "Keen Gaming", "Newbee", "Infamous", "Chaos Esports", "Natus Vincere", "Royal Never Give Up", "Mineski"
+$teams = "Team Secret", "Virtus Pro", "Vici Gaming", "Evil Geniuses", "Team Liquid", "PSG.LGD", "Fnatic", "TNC Predator", "OG", "Keen Gaming", "Newbee", "Infamous", "Natus Vincere", "Royal Never Give Up", "Mineski"
 python .\download.py $teams
