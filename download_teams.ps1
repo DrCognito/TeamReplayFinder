70c312397f1ba3ef1065522bfa102b9d6655312a
Matt Chadwick <dr.m.chadwick@gmail.com>
2020-07-17 01:26:18 +0100

    Use .venv folder Fix $teams formating.

--- download_teams.ps1
+++ download_teams.ps1
@@ -1,3 +1,3 @@
-C:/Users/Cognito/.virtualenvs/TeamReplayFinder-8Br9gCsa/Scripts/activate.ps1
-$teams = 1883502 36 111474 6214973 2586976 6685591 7554697 6953913
+.\.venv\Scripts\activate.ps1
+$teams = "1883502", "36", "111474", "6214973", "2586976", "6685591", "7554697", "6953913"
 python .\download.py $teams
