428e7281c28f093709ca3bf246ac00b487724681
Matt Chadwick <dr.m.chadwick@gmail.com>
2020-08-10 00:30:05 +0100

    updated nip added 5men

--- load_old_teams.py
+++ load_old_teams.py
@@ -54,9 +54,9 @@ ValidityTime['Ninjas in Pyjamas'] = datetime.datetime(2020, 4, 22, 0, 0, 0, 0)
 _TeamIDs['Ninjas in Pyjamas'] = 6214973
 
 PlayerIDs['OG'] = collections.OrderedDict()
-PlayerIDs['OG']['SumaiL'] = 76561198071885769
-PlayerIDs['OG']['Topson'] = 76561198054320440
 PlayerIDs['OG']['midone'] = 76561198076851106
+PlayerIDs['OG']['Topson'] = 76561198054320440
+PlayerIDs['OG']['Ceb'] = getSteam64From32(88271237)
 PlayerIDs['OG']['Saksa'] = 76561198064001473
 PlayerIDs['OG']['N0tail'] = 76561197979938082
 ValidityTime['OG'] = datetime.datetime(2020, 1, 29, 0, 0, 0, 0)
@@ -98,6 +98,53 @@ PlayerIDs['VP.Prodigy']['Save'] = 76561198278146366
 PlayerIDs['VP.Prodigy']['eine'] = 76561198089337610
 ValidityTime['VP.Prodigy'] = datetime.datetime(2020, 4, 1, 0, 0, 0, 0)
 _TeamIDs['VP.Prodigy'] = 7819701
+
+#Queried at 2020-08-05T00:23:45.903310
+PlayerIDs['Evil Geniuses'] = collections.OrderedDict()
+PlayerIDs['Evil Geniuses']['Arteezy'] = getSteam64From32(86745912)
+PlayerIDs['Evil Geniuses']['Abed'] = 76561198114980808
+PlayerIDs['Evil Geniuses']['RAMZES666'] = 76561198093117099
+PlayerIDs['Evil Geniuses']['Cr1t'] = 76561197986172872
+PlayerIDs['Evil Geniuses']['Fly'] = 76561198054420884
+ValidityTime['Evil Geniuses'] = datetime.datetime(2019, 9, 16, 0, 0, 0, 0)
+_TeamIDs['Evil Geniuses'] = 39
+
+PlayerIDs['Team Secret'] = collections.OrderedDict()
+PlayerIDs['Team Secret']['MATUMBAMAN'] = 76561198032578355
+PlayerIDs['Team Secret']['Nisha'] = 76561198082035378
+PlayerIDs['Team Secret']['zai'] = 76561198033828054
+PlayerIDs['Team Secret']['YapzOr'] = 76561198049382766
+PlayerIDs['Team Secret']['Puppey'] = 76561198047544485
+ValidityTime['Team Secret'] = datetime.datetime(2019, 11, 4, 0, 0, 0, 0)
+_TeamIDs['Team Secret'] = 1838315
+
+PlayerIDs['Team Liquid'] = collections.OrderedDict()
+PlayerIDs['Team Liquid']['miCKe'] = 76561198113227791
+PlayerIDs['Team Liquid']['qojqva'] = 76561198047004422
+PlayerIDs['Team Liquid']['Boxi'] = 76561198037756242
+PlayerIDs['Team Liquid']['Taiga'] = 76561198362058302
+PlayerIDs['Team Liquid']['iNSaNiA'] = 76561198014846690
+ValidityTime['Team Liquid'] = datetime.datetime(2019, 10, 2, 0, 0, 0, 0)
+_TeamIDs['Team Liquid'] = 2163
+
+#Queried at 2020-08-10T00:26:49.043267
+PlayerIDs['5men'] = collections.OrderedDict()
+PlayerIDs['5men']['Ace'] = getSteam64From32(97590558)
+PlayerIDs['5men']['Chessie'] = getSteam64From32(172424257)
+PlayerIDs['5men']['Xibbe'] = getSteam64From32(50580004)
+PlayerIDs['5men']['MISERY'] = getSteam64From32(87382579)
+PlayerIDs['5men']['pieliedie'] = getSteam64From32(6922000)
+ValidityTime['5men'] = datetime.datetime(2019, 9, 14, 0, 0, 0, 0)
+_TeamIDs['5men'] = 8077174
+
+PlayerIDs['Ninjas in Pyjamas'] = collections.OrderedDict()
+PlayerIDs['Ninjas in Pyjamas']['charlie'] = 76561198082315226
+PlayerIDs['Ninjas in Pyjamas']['Supream'] = 76561198018778775
+PlayerIDs['Ninjas in Pyjamas']['SabeRLighT'] = 76561198086478594
+PlayerIDs['Ninjas in Pyjamas']['Era'] = 76561198060583478
+PlayerIDs['Ninjas in Pyjamas']['Misha'] = getSteam64From32(90125566)
+ValidityTime['Ninjas in Pyjamas'] = datetime.datetime(2020, 4, 22, 0, 0, 0, 0)
+_TeamIDs['Ninjas in Pyjamas'] = 6214973
 
 path = None
 engine = InitTeamDB(path)
