6de0aae1513dd19622efd03dff7e40d3c7c75ada
Matthew Chadwick <dr.m.chadwick@gmail.com>
2021-11-17 00:39:17 +0000

    Conversion for 64 to 32. Is 64bit bool.

--- util.py
+++ util.py
@@ -1,5 +1,16 @@
 def convert_to_64_bit(number):
     min64b = 76561197960265728
-    if number < min64b:
+    if not is_64bit(number):
         return number + min64b
     return number
+
+def convert_to_32_bit(number):
+    min64b = 76561197960265728
+    if is_64bit(number):
+        return number - 76561197960265728
+    return number
+
+
+def is_64bit(number):
+    min64b = 76561197960265728
+    return (number >= min64b)
