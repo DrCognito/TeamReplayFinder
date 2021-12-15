3e0fdaa5a7c6e3b7ab0d03c88d22272002ffdb06
Matthew Chadwick <dr.m.chadwick@gmail.com>
2021-12-15 17:16:51 +0000

    late 2021 processing update

--- current_teams.ps1
+++ current_teams.ps1
@@ -1,13 +1,10 @@
 .\.venv\Scripts\activate.ps1
-$nip = @(6214973)
-$summit_ga = "8161113", "7422789", "2576071", "8121295"
-$summit_gb = $nip + "8077174", "8112124", "7453744"
-$summit_gs = "2163", "6685591"
-$summit = $summit_ga + $summit_gb + $summit_gs
-$epic_extra_gb = "7556672"
-$epic_playoff = "7556672", "8121295", "2576071", "8161113", "6209143"
-$epic_div2 = "8077174", "6214973", "6209143", "7556672", "46", "8161113", "7118032", "2576071", "7486089", "5014799"
-$teams = $epic_div2
+$og = @("2586976")
+$DPC_WEU_T1_D1 = "7554697", "111474", "2163", "1838315", "8291895", "8599101", "8598999"
+$OGA_DOTAPIT_S5_EU = "7119388", "36", "8291895", "7554697", "2163", "1838315", "111474"
+
+$teams = $og + $DPC_WEU_T1_D1 + $OGA_DOTAPIT_S5_EU | Get-Unique
+Write-Output "Processing " $teams
 
 python .\team_parse.py $teams
-python .\download.py $teams
+python .\download.py $teams 
