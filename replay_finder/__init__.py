7b841e46a578fe9aac8aeab561a29663985b1d50
Matthew Chadwick <dr.m.chadwick@gmail.com>
2022-02-07 17:58:25 +0000

    Add datdota api limit constant.

--- replay_finder/__init__.py
+++ replay_finder/__init__.py
@@ -11,3 +11,6 @@ load_dotenv(dotenv_path="setup.env")
 WEB_API_LIMIT = 100000
 # Unkown if it is this low, reported by dota2 docs.
 GC_API_LIMIT = 100
+# Datdota limit of 500 a day
+# https://www.datdota.com/about
+DATDOTA_API_LIMIT = 500
