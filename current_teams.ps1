ef09f60fff814df8cb3dc32641eee6edbfdee33c
Matthew Chadwick <dr.m.chadwick@gmail.com>
2022-02-07 17:56:49 +0000

    DPC_WEU_Final teams update. Nicer print out for teams list.

--- current_teams.ps1
+++ current_teams.ps1
@@ -2,9 +2,13 @@
 $og = @("2586976")
 $DPC_WEU_T1_D1 = "7554697", "111474", "2163", "1838315", "8291895", "8599101", "8598999"
 $OGA_DOTAPIT_S5_EU = "7119388", "36", "8291895", "7554697", "2163", "1838315", "111474"
+$DPC_WEU_FINAL = "2163", "8599101", "8291895"
 
-$teams = $og + $DPC_WEU_T1_D1 + $OGA_DOTAPIT_S5_EU | Get-Unique
-Write-Output "Processing " $teams
+$teams = $og + $DPC_WEU_FINAL | Get-Unique
+# $teams = "1838315", "2586976", "8291895", "7554697"
+Write-Output("Updating replay-list for: " + $teams -join " ")
 
 python .\team_parse_datdota.py $teams
-python .\download.py $teams 
+python .\download.py $teams
+Write-Output "Downloading ogscrims from googledrive"
+rclone copy -P ogscrims: E:\Dota2\dbScrim\
