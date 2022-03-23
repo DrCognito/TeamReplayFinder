521e31b91634314c63dab7880014e862b2c93428
Matthew Chadwick <dr.m.chadwick@gmail.com>
2022-03-23 17:04:36 +0000

    Added a timedelta for gap between replay download tries. Added a timedelta for giving up attempts for downloads.

--- replay_finder/__init__.py
+++ replay_finder/__init__.py
@@ -3,6 +3,7 @@
 # Steam GC api
 
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv(dotenv_path="setup.env")
 
@@ -14,3 +15,7 @@ GC_API_LIMIT = 100
 # Datdota limit of 500 a day
 # https://www.datdota.com/about
 DATDOTA_API_LIMIT = 500
+
+REPLAY_DOWNLOAD_DELAY = timedelta(minutes=5)
+# The message on dotabuff says that replays missing for 3 hours rarely appear.
+REPLAY_DOWNLOAD_GIVEUP = timedelta(hours=3)
